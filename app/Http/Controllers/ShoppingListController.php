89daf9616b0b5e5f5cc1e40e4d850beff9b551e3
Marco Consiglio <m.consiglio@sermetra.it>
2020-03-06 09:07:02 +0100

    Aggiunta la risorsa ShoppingList.

---
+++ app/Http/Controllers/ShoppingListController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ShoppingList;
+use Illuminate\Http\Request;
+
+class ShoppingListController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ShoppingList  $shoppingList
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ShoppingList $shoppingList)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ShoppingList  $shoppingList
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ShoppingList $shoppingList)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ShoppingList  $shoppingList
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ShoppingList $shoppingList)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ShoppingList  $shoppingList
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ShoppingList $shoppingList)
+    {
+        //
+    }
+}
---
+++ database/factories/ShoppingListFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\ShoppingList;
+use Faker\Generator as Faker;
+
+$factory->define(ShoppingList::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
