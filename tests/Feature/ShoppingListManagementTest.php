ada26ed32f728f46a3c023d7464626c7f4fce48f
Marco Consiglio <mrccnsgl@gmail.com>
2020-03-14 21:18:51 +0100

    Test "un utente può modificare una lista della spesa".

--- tests/Feature/ShoppingListManagementTest.php
+++ tests/Feature/ShoppingListManagementTest.php
@@ -10,6 +10,7 @@ use App\ShoppingList;
 
 class ShoppingListManagementTest extends TestCase
 {
+    use WithFaker;
     use RefreshDatabase;
 
     /**
@@ -18,7 +19,7 @@ class ShoppingListManagementTest extends TestCase
      */
     public function a_user_can_view_shopping_lists_index()
     {
-        $this->withoutExceptionHandling();
+        // $this->withoutExceptionHandling();
         // Arrange
         $user = factory(User::class)->create();
         $shopping_list = $user->shopping_lists()->save(factory(ShoppingList::class)->make());
@@ -38,6 +39,7 @@ class ShoppingListManagementTest extends TestCase
      */
     public function a_user_can_view_shopping_list_items()
     {
+        // $this->withoutExceptionHandling();
         // Arrange
         $user = factory(User::class)->create();
         $shopping_list = $user->shopping_lists()->save(factory(ShoppingList::class)->make());
@@ -50,6 +52,32 @@ class ShoppingListManagementTest extends TestCase
         $response->assertViewIs("shopping_list.show");
     }
 
+
+    /**
+     * A User can edit ShoppingList.
+     * @test
+     */
+    public function a_user_can_edit_shopping_list()
+    {
+        $this->withoutExceptionHandling();
+        // Arrange
+        $user = factory(User::class)->create();
+        $shopping_list =
+            $user->shopping_lists()
+                 ->save(
+                     factory(ShoppingList::class)->make()
+                 );
+        $this->be($user);
+
+        // Act
+        $shopping_list->title = $this->faker->sentence(4);
+        $response = $this->get(route("shopping_list.update", $shopping_list));
+
+        // Assert
+        $response->assertViewIs("shopping_list.index");
+        $attributes = $shopping_list->toArray();
+        unset($attributes["updated_at"]);
+        $this->assertDatabaseHas($shopping_list->table_name, $shopping_list->raw());
     }
 
 }
