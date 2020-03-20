48fc55ede0fb2ea7151a97288ae25ff9a0114203
Marco Consiglio <mrccnsgl@gmail.com>
2020-03-20 08:58:30 +0100

    Test: Un utente può vedere la sua collezione di liste della spesa.

---
+++ tests/Browser/ShoppingListTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+use App\{User, ShoppingList};
+
+class ShoppingListTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * Un utente può vedere una collezione delle sue liste della spesa.
+     * @test
+     */
+    public function a_user_can_view_shopping_lists_collection()
+    {
+        $this->withoutExceptionHandling();
+        // Arrange
+        $user = factory(User::class)->create();
+        $shopping_list = $user->shopping_lists()->save(
+            factory(ShoppingList::class)->make()
+        );
+
+        // Act
+        $this->browse(function (Browser $browser) use($user, $shopping_list) {
+            $browser->loginAs($user)
+                    ->visit(route("shopping_list.index"))
+                    ->assertSee($shopping_list->title);
+        });
+    }
+}
