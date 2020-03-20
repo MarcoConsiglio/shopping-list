09be80fe0be37f1e1b60d689456426179f6995b7
Marco Consiglio <mrccnsgl@gmail.com>
2020-03-20 08:43:41 +0100

    Migliorata la visualizzazione della collezione di liste della spesa.

--- resources/views/shopping_list/index.blade.php
+++ resources/views/shopping_list/index.blade.php
@@ -3,8 +3,8 @@
 <h3 class="text-center display-4"></h3>
 @foreach ($shopping_lists as $shopping_list)
 <a href="{{route("shopping_list.show", $shopping_list)}}">
-    <div class="card-body">
-        <div class="card">
+    <div class="card shopping-list">
+        <div class="card-body">
             <h4 class="card-title">{{$shopping_list->title}}</h4>
         </div>
     </div>
