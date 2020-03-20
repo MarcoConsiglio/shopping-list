dedf6bbb1f1f8d0054738dee3ffc4e5bf9e49699
Marco Consiglio <mrccnsgl@gmail.com>
2020-03-20 08:54:13 +0100

    Sembra che dusk se ne fotta allegramente della configurazione del database.

--- config/database.php
+++ config/database.php
@@ -43,13 +43,6 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
-        'dusk' => [
-            'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', ":memory:"),
-            'prefix' => '',
-            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
-        ],
-
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
