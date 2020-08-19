6fb603cce1b9639c845315d0a26d31043a9a9e1f
Marco Consiglio <m.consiglio@sermetra.it>
2020-08-19 13:55:19 +0200

    Migliorata l'estetica dell'indice delle liste della spesa.

--- resources/views/shopping_list/index.blade.php
+++ resources/views/shopping_list/index.blade.php
@@ -9,25 +9,34 @@
     <h4 class="text-center display-4">Le tue liste della spesa</h4>
     <div class="d-flex justify-content-around">
         <!-- ShoppingList Card -->
+        <ul class="list-group list-group-flush">
         @foreach($shopping_lists as $shopping_list)
-        <div class="card shopping-list">
-            <div class="card-body d-flex">
+            <li class="list-group-item mb-3 shadow-sm rounded">
                 <a dusk="shopping_list_{{$shopping_list->id}}" href="{{route("shopping_list.show", $shopping_list)}}">
-                    <h4 class="card-title text-truncate overflow-hidden" style="width: 160px">{{$shopping_list->title}}</h4>
+                    <h4 class="text-truncate overflow-hidden">{{$shopping_list->title}}</h4>
                 </a>
-            </div>
-            <div class="card-footer">
-                <div class="d-flex justify-content-around">
-                    <a dusk="delete_button_{{$shopping_list->id}}" class="btn btn-danger text-white" href="#" role="button" data-toggle="modal" data-target="#delete_modal_{{$shopping_list->id}}">
+                <div class="d-flex justify-content-end">
+                    <a dusk="delete_button_{{$shopping_list->id}}" class="btn btn-danger text-white mx-2 shadow-sm" href="#" role="button" data-toggle="modal" data-target="#delete_modal_{{$shopping_list->id}}">
                         <i class="fas fa-trash-alt"></i>
                     </a>
-                    <a dusk="edit_button_{{$shopping_list->id}}" class="pr-2 btn btn-warning text-white" href="#" role="button" data-toggle="modal" data-target="#edit_modal_{{$shopping_list->id}}">
+                    <a dusk="edit_button_{{$shopping_list->id}}" class="pr-2 btn btn-warning text-white mx-2 shadow-sm  " href="#" role="button" data-toggle="modal" data-target="#edit_modal_{{$shopping_list->id}}">
                         <i class="fas fa-edit"></i>
                     </a>
                 </div>
+            </li>
+        {{-- <div>
+
+            <div class="d-flex justify-content-around">
+                <a dusk="delete_button_{{$shopping_list->id}}" class="btn btn-danger text-white" href="#" role="button" data-toggle="modal" data-target="#delete_modal_{{$shopping_list->id}}">
+                    <i class="fas fa-trash-alt"></i>
+                </a>
+                <a dusk="edit_button_{{$shopping_list->id}}" class="pr-2 btn btn-warning text-white" href="#" role="button" data-toggle="modal" data-target="#edit_modal_{{$shopping_list->id}}">
+                    <i class="fas fa-edit"></i>
+                </a>
             </div>
-        </div>
+        </div> --}}
         @endforeach
+        </ul><!-- .list-group .list-group-flush -->
     </div>
     @include("shopping_list.button.create")
 @endsection
