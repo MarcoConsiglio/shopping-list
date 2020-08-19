ec04f7fdb5b803ec93a61ead8f10ac6ec8a36334
Marco Consiglio <m.consiglio@sermetra.it>
2020-08-19 14:07:10 +0200

    Migliorato il tasto Modifica dei prodotti.

--- resources/views/product/button/edit.blade.php
+++ resources/views/product/button/edit.blade.php
@@ -2,6 +2,6 @@
     dusk="edit_product_{{$product->id}}"
     data-toggle="modal"
     data-target="#editProductModal_{{$product->id}}"
-    class="btn btn-warning">
-    <i class="fas fa-edit text-white pr-0"></i>
+    class="btn btn-warning pr-2">
+    <i class="fas fa-edit text-white"></i>
 </a >
