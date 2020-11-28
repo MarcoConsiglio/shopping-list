0c0b34f16b01f29ba6a62b40ac8ffbe3244c7371
vagrant <vagrant@homestead>
2020-11-28 13:37:48 +0000

    Refactoring del ProductTest.

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -10,6 +10,61 @@ use App\{User, ShoppingList, Product};
 class ProductTest extends TestCase
 {
     use RefreshDatabase;
+
+    /**
+     * A User of this app.
+     *
+     * @var \App\User
+     */
+    private $user;
+
+    /**
+     * A ShoppingList.
+     *
+     * @var \App\ShoppingList
+     */
+    private $shopping_list;
+
+    /**
+     * A Product.
+     *
+     * @var \App\Product
+     */
+    private $product;
+
+    /**
+     * Set up.
+     *
+     * @param boolean $make_user            Weather to make a User instead of persist it.
+     * @param boolean $make_shopping_list   Weather to make a ShoppingList instead of persist it.
+     * @param boolean $make_product         Weather to make a Product instead of persist it.
+     * @return void
+     */
+    protected function setUp(
+        bool $make_user = false,
+        bool $make_shopping_list = false,
+        bool $make_product = false): void
+    {
+        parent::setUp();
+
+        // Set models.
+        $make_user ?
+            $this->user = factory(User::class)->make() :
+            $this->user = factory(User::class)->create();
+
+        $make_shopping_list ?
+            $this->shopping_list = factory(ShoppingList::class)->make() :
+            $this->shopping_list = factory(ShoppingList::class)->create([
+                "user_id" => $this->user->id
+            ]);
+
+        $make_product ?
+            $this->product = factory(Product::class)->make() :
+            $this->product = factory(Product::class)->create([
+                "shopping_list_id" => $this->shopping_list->id
+            ]);
+    }
+
     /**
      * Un utente può aggiungere un prodotto ad una lista della spesa.
      * @test
@@ -18,22 +73,24 @@ class ProductTest extends TestCase
     {
         // $this->withoutExceptionHandling();
         // Arrange
-        $user = factory(User::class)->create();
-        $shopping_list = factory(ShoppingList::class)->make();
-        $user->shopping_lists()->save($shopping_list);
-        $product = factory(Product::class)->make();
-        $attributes = $product->getAttributes();
+        $this->setUp(false, false, true);
 
         // Act
-        $response = $this->actingAs($user)
-                         ->post(route("shopping_list.product.store", $shopping_list), $attributes);
+        $response = $this->actingAs($this->user)
+                         ->post(
+                             route(
+                                 "shopping_list.product.store",
+                                 $this->shopping_list
+                             ),
+                             $this->product->attributesToArray()
+                         );
 
         // Re-arrenge
-        $attributes = $product->refresh()->getAttributes();
+        $attributes = $this->product->refresh()->getAttributes();
 
         // Assert
         $this->assertDatabaseHas("products", $attributes);
-        $response->assertRedirect(route("shopping_list.show", $shopping_list));
+        $response->assertRedirect(route("shopping_list.show", $this->shopping_list));
     }
 
     /**
@@ -42,21 +99,18 @@ class ProductTest extends TestCase
      */
     public function a_user_can_delete_a_product()
     {
-        // Arrange
-        $user = factory(User::class)->create();
-        $shopping_list = factory(ShoppingList::class)->make();
-        $user->shopping_lists()->save($shopping_list);
-        $product = factory(Product::class)->make();
-        $shopping_list->refresh();
-        $shopping_list->products()->save($product);
+        // Arrange in setUp()
 
         // Act
-        $response = $this->actingAs($user)
-                         ->delete(route("shopping_list.product.destroy", [$shopping_list, $product]));
+        $response = $this->actingAs($this->user)
+                         ->delete(
+                             route("shopping_list.product.destroy",
+                             [$this->shopping_list, $this->product])
+                         );
 
         // Assert
-        $this->assertDatabaseMissing("products", $product->getAttributes());
-        $response->assertRedirect(route("shopping_list.show", $shopping_list));
+        $this->assertDatabaseMissing("products", $this->product->getAttributes());
+        $response->assertRedirect(route("shopping_list.show", $this->shopping_list));
     }
 
     /**
@@ -66,25 +120,22 @@ class ProductTest extends TestCase
     public function a_user_can_edit_a_product()
     {
         // $this->withoutExceptionHandling();
-        // Arrange
-        $user = factory(User::class)->create();
-        $shopping_list = factory(ShoppingList::class)->make();
-        $user->shopping_lists()->save($shopping_list);
-        $shopping_list->refresh();
-        $product = factory(Product::class)->make();
-        $shopping_list->products()->save($product);
-        $product->refresh();
+        // Arrange in setUp()
         $edited_product = factory(Product::class)->make();
-        $edited_product->id = $product->id;
 
         // Act
-        $response = $this->actingAs($user)
-                         ->put(route("shopping_list.product.update", [$shopping_list, $product]),
-                               $edited_product->getAttributes());
+        $response = $this->actingAs($this->user)
+                         ->put(
+                             route(
+                                 "shopping_list.product.update",
+                                 [$this->shopping_list, $this->product]
+                             ),
+                             $edited_product->getAttributes()
+                         );
 
         // Assert
         $this->assertdatabaseHas("products", $edited_product->getAttributes());
-        $response->assertRedirect(route("shopping_list.show", $shopping_list));
+        $response->assertRedirect(route("shopping_list.show", $this->shopping_list));
     }
 
     /**
@@ -92,31 +143,29 @@ class ProductTest extends TestCase
      * @test
      */
     public function a_user_can_add_a_product_to_the_cart() {
-        // Arrange
-        $user = factory(User::class)->create();
-        $shopping_list = factory(ShoppingList::class)->make();
-        $user->shopping_lists()->save($shopping_list);
-        $shopping_list->refresh();
-        $product = factory(Product::class)->make();
-        $shopping_list->products()->save($product);
-        $added_quantity = $product->quantity;
+        // Arrange in setUp()
+        $added_quantity = $this->product->quantity;
 
         // Act
-        $response = $this->actingAs($user)
-                         ->post(route("shopping_list.product.add_to_cart",
-                                      compact(["product", "shopping_list"])),
-                         ["cart_quantity" => $added_quantity]);
-        $product->refresh();
+        $response = $this->actingAs($this->user)
+                         ->post(
+                             route(
+                                 "shopping_list.product.add_to_cart",
+                                 [$this->shopping_list, $this->product]
+                             ),
+                             ["cart_quantity" => $added_quantity]
+                         );
+        $this->product->refresh();
 
         // Assert
-        $attributes = $product->attributesToArray();
+        $attributes = $this->product->attributesToArray();
         $attributes["cart_quantity"] = (float)$added_quantity;
         unset(
             $attributes["created_at"],
             $attributes["updated_at"]
         );
         $this->assertDatabaseHas("products", $attributes);
-        $response->assertRedirect(route("shopping_list.show", $shopping_list));
+        $response->assertRedirect(route("shopping_list.show", $this->shopping_list));
     }
 
     /**
@@ -124,20 +173,18 @@ class ProductTest extends TestCase
      * @test
      */
     public function a_user_cant_add_negative_quantity_to_the_cart() {
-        // Arrange
-        $user = factory(User::class)->create();
-        $shopping_list = factory(ShoppingList::class)->make();
-        $user->shopping_lists()->save($shopping_list);
-        $shopping_list->refresh();
-        $product = factory(Product::class)->make();
-        $shopping_list->products()->save($product);
+        // Arrange in setUp()
 
         // Act
-        $response = $this->actingAs($user)
-        ->post(route("shopping_list.product.add_to_cart",
-                     compact(["product", "shopping_list"])),
-        ["cart_quantity" => -5]);
-        $product->refresh();
+        $response = $this->actingAs($this->user)
+        ->post(
+            route(
+                "shopping_list.product.add_to_cart",
+                [$this->shopping_list, $this->product]
+            ),
+            ["cart_quantity" => -5]
+        );
+        $this->product->refresh();
 
         // Assert
         $response->assertSessionHasErrors(["cart_quantity"]);
