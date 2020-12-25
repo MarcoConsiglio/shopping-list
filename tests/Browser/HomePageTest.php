2b102f785479e8c229dfd151357a18717bfa2265
Marco Consiglio <m.consiglio@sermetra.it>
2020-12-25 13:43:55 +0100

    #41 Un utente loggato può accedere dalla home alle sue liste della spesa.

--- tests/Browser/HomePageTest.php
+++ tests/Browser/HomePageTest.php
@@ -2,12 +2,15 @@
 
 namespace Tests\Browser;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
 class HomePageTest extends DuskTestCase
 {
+    use DatabaseMigrations;
+
    /**
     * Gli utenti ospiti devono poter vedere la homepage.
     * @test
@@ -17,6 +20,22 @@ class HomePageTest extends DuskTestCase
         $this->browse(function (Browser $browser) {
             $browser->visit('/')
                     ->assertSee('by Marco Consiglio');
+        });
+    }
+
+    /**
+     * Un utente può accedere all'indice delle liste dalla
+     * homepage.
+     * @test
+     */
+    public function a_user_can_access_shopping_list_index() {
+        $user = User::factory()->create();
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->loginAs($user)
+                    ->visit('/')
+                    ->assertSeeLink('Le tue liste')
+                    ->clickLink('Le tue liste')
+                    ->assertRouteIs("shopping_list.index");
         });
     }
 }
