6d30bb05109b067ec41371452c0835dd833d7571
Marco Consiglio <m.consiglio@sermetra.it>
2020-12-30 17:37:57 +0100

    #45 Aggiunto il numero di versione in homepage.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.standard')
 @section('content')
-    <div class="d-flex flex-column">
+    <div class="d-flex flex-column text-center">
         <img class="img-fluid mx-auto" style="width: 219px" src="{{asset('images/logo.png')}}">
-        <cite class="text-center">by Marco Consiglio</cite>
+        <cite>by Marco Consiglio</cite>
+        <cite>v{{config("version")}}</cite>
     </div>
 @endsection
