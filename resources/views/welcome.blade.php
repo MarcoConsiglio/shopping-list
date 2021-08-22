56890ea6ca8f00ba860b0c1ee057649b9f4ddf45
Marco Consiglio <mrccnsgl@gmail.com>
2021-08-22 18:02:47 +0000

    #46 Aggiunti i pulsanti di login e registrazione alla homepage.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,10 @@
 @section('content')
     <div class="d-flex flex-column text-center">
         <img class="img-fluid mx-auto" style="width: 219px" src="{{asset('images/logo.png')}}">
+        <div class="d-flex flex-row justify-content-center">
+            <a class="btn btn-outline-primary" href="{{route("register")}}">{{__("Registrati")}}</a>
+            <a class="btn btn-success ml-3" href="{{route("login")}}">{{__("Login")}}</a>
+        </div>
         <cite>by Marco Consiglio</cite>
         <cite>v{{config("version")}}</cite>
     </div>
